3756a829043679b677cff7341ea385a9fdf3af7a
Érica Joice dos Santos <ejoice19@gmail.com>
2022-06-29 23:18:59 -0300

    pomodoro finish

--- contador.js
+++ contador.js
@@ -1,18 +1,92 @@
-//let pomodoroBtn = document.getElementById('pomodoroBtn');
-//let pausaCurtaBtn = document.getElementById('pausaCurtaBtn');
-//let pausaLongaBtn = document.getElementById('pausaLongaBtn');
-let time = document.getElementById('counter')
-let startBtn = document.getElementById('start-btn');
-
+const alarm = new Audio('clock-alarm.mp3')
 const timer = {
-   pomodoro: 25,
+   pomodoro:  25,
    shortBreak: 5,
    longBreak: 15,
    longBreakInterval: 4,
+   sessions: 0,
 };
+let interval;
 
 const menuButtons = document.querySelector('#mode-buttons');
 menuButtons.addEventListener('click', handleMode);
+
+const startBtn = document.getElementById('start-btn');
+startBtn.addEventListener('click', () => {
+   const { action } = startBtn.dataset;
+   if (action === 'start') {
+      startTimer();
+   }else{
+      stopTimer();
+   }   
+});
+
+//*function will calculate the difference between the end and start, and convert by time
+function getRemainingTime(endTime) { 
+   
+   const currentTime = Date.parse(new Date());
+   const difference = endTime - currentTime;
+
+   const total = Number.parseInt(difference / 1000, 10);
+   const minutes = Number.parseInt((total / 60) % 60, 10);
+   const seconds = Number.parseInt(total % 60, 10);
+
+   return {
+      total, 
+      minutes,
+      seconds,
+   }
+}
+
+function startTimer() {
+   let { total } = timer.remainingTime; //*when i use {time} is calling the property inside the object;
+   const endTime = Date.parse(new Date()) + total * 1000;
+
+   if (timer.mode === 'pomodoro') timer.sessions++;
+
+   startBtn.dataset.action = 'stop';
+   startBtn.textContent = 'Pausar';
+   startBtn.classList.add('active')
+
+   interval =setInterval(function(){
+      timer.remainingTime = getRemainingTime(endTime);
+      updateTimer();
+
+      total = timer.remainingTime.total;
+      if (total <= 0) {
+         clearInterval(interval)
+      
+
+      switch(timer.mode){
+         case 'pomodoro':
+            if (timer.sessions % timer.longBreakInterval === 0) {
+               switchMode('longBreak');
+            } else {
+               switchMode('shortBreak');
+            }
+            break;
+         default:
+               switchMode('pomodoro')
+      }
+
+      document.querySelector(`[data-sound="${timer.mode}"]`).play();
+
+      startTimer();
+      }
+   }, 1000);
+
+   //*setInterval is method that update which 1 second(1000 miliseconds)
+}
+
+function stopTimer() {
+   clearInterval(interval);
+
+   startBtn.dataset.action = 'start';
+   startBtn.textContent = 'Iniciar';
+   startBtn.classList.remove('active');
+}
+
+
 
 function updateTimer() {
    const { remainingTime } = timer;
@@ -26,7 +100,6 @@ function updateTimer() {
    
 }
 
-
 //*Buttons are working and changing the mode
 function switchMode(mode) {
    timer.mode = mode;
@@ -36,10 +109,9 @@ function switchMode(mode) {
       seconds: 0,
    };
 
-
    document
-    .querySelectorAll('button[data-mode]')
-    .forEach(e => e.classList.remove('active')); //!Remove active from all buttons
+   .querySelectorAll('button[data-mode]')
+   .forEach(e => e.classList.remove('active')); //!Remove active from all buttons
    document.querySelector(`[data-Mode="${mode}"]`).classList.add('active');//! Add active only at the button wich was clicked
    document.body.style.backgroundColor = `var(--${mode})` //!Add Color when the button is clicked
 
@@ -52,7 +124,13 @@ function handleMode(event) {
    if (!mode) return;
 
    switchMode(mode);
+   stopTimer();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+   switchMode('pomodoro');
+   stopTimer();
+ });
 
 
 
